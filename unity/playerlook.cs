0e82c7a936b9a1f038edb46d80634ab45b734b29
Jordan <jsm54208@gmail.com>
2019-11-16 15:56:43 -0600

    unity added from home computer

---
+++ unity/playerlook.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerlook : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity;
+    [SerializeField] private Transform playerBody;
+    float yaw;
+    float pitch;
+    private float xAxisClamp = 0.0f;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        LockCursor();
+    }
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update()
+    {
+        CameraRotation();
+    }
+    private void CameraRotation()
+    {
+        yaw = mouseSensitivity * Input.GetAxis("Mouse X") * Time.deltaTime;
+        pitch = mouseSensitivity * Input.GetAxis("Mouse Y") * Time.deltaTime;
+
+        xAxisClamp += pitch;
+
+        if(xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            pitch = 0.0f;
+            ClampXaxisToValue(270.0f);
+        }
+        else if (xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            pitch = 0.0f;
+            ClampXaxisToValue(90.0f);
+        }
+        transform.Rotate(Vector3.left * pitch);
+        playerBody.Rotate(Vector3.up * yaw);
+    }
+
+    private void ClampXaxisToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+}
---
+++ unity/rayCast.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rayCast : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    // Update is called once per frame
+    public ParticleSystem muzzleFlash;
+    public GameObject particals;
+    public float impactForce;
+    public float fireRate = 15f;
+
+    private float NextTimeToFire = 0f;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) && Time.time >= NextTimeToFire)
+        {
+            NextTimeToFire = Time.time + 1f / fireRate;
+            AimNshoot();
+        }
+        
+    }
+    void AimNshoot()
+    {
+        muzzleFlash.Play();
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, range)){
+            Debug.Log(hit.transform.name);
+            Attack_life target = hit.transform.GetComponent<Attack_life>();
+            if(target != null){
+
+                target.TakeDamage(damage);
+            }
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+            GameObject inmpactGo = Instantiate(particals, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(inmpactGo, 2f);
+        }
+    }
+}
